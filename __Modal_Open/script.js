56315c2c501d7ab59c7e55c9e279a026c05d225e
Saurabh-Chede <saurabhchede21@gmail.com>
2026-01-08 15:25:13 +0530

    _002

---
+++ __Modal_Open/script.js
@@ -0,0 +1,12 @@
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+
+function openModal() {
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal() {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
